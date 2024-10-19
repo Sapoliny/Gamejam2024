03fc6195d7e4a7cc9611c3825c96c75f30d3f194
FLKS07 <67174547+FLKS07@users.noreply.github.com>
2024-10-19 11:14:01 +0100

    out 19 11:13

--- Assets/Scripts/Enemies/BulletShooter/Shooter.cs
+++ Assets/Scripts/Enemies/BulletShooter/Shooter.cs
@@ -4,19 +4,63 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public GameObject bullet;
+    public GameObject[] bulletPrefabs;
 
+    private Transform playerTransform;
+
+    public float bulletSpeed;
+
+    [Tooltip("Fire in seconds")]
     public float fireRate;
+
+    private bool canShoot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        canShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canShoot)
+        {
+            Shoot();
+            canShoot = false;
+            StartCoroutine(shootTiming(fireRate));
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 direction = playerTransform.position - this.transform.position;
+        float angle = Vector2.Angle(Vector2.left, direction);
+        if (direction.y > 0)
+        {
+            angle *= -1;
+        }
+        Debug.Log(angle);
+
+        GameObject bullet = Instantiate(getRandomPrefab(), this.transform.position, Quaternion.Euler(0, 0, angle));
+
+
+        direction.Normalize();
+
+        Rigidbody2D rb2d = bullet.GetComponent<Rigidbody2D>();
+        rb2d.velocity = direction * bulletSpeed;
+    }
+
+    IEnumerator shootTiming(float fireRate)
+    {
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+    }
+
+    GameObject getRandomPrefab() 
+    { 
+        int maxIndex = bulletPrefabs.Length - 1;
+        return bulletPrefabs[Random.Range(0, maxIndex)];
     }
 }
