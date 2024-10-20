c9fe832bcc3637b311507986e0b04bad00919211
FLKS07 <67174547+FLKS07@users.noreply.github.com>
2024-10-20 12:11:33 +0100

    out 20 12:11

--- Assets/Scripts/Enemies/Boss2.cs
+++ Assets/Scripts/Enemies/Boss2.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Boss2 : MonoBehaviour
@@ -11,12 +13,16 @@ public class Boss2 : MonoBehaviour
     public GameObject gameManager;
 
     Vector3 defaultPosition;
-    public Transform[] swipePositions;
+
+    public Transform[] swipePositionsLeft;
+    public Transform[] swipePostionsRight;
+
     public Transform exitScreen;
     public float movementSpeedExit;
     public float movementSpeed;
 
     public float swipeDamage;
+
 
     bool waitingToEnd = false;
 
@@ -75,50 +81,66 @@ public class Boss2 : MonoBehaviour
         waitingToEnd = true;
     }
 
-    IEnumerator attack2()
+        IEnumerator attack2()
+        {
+            while (true)
+            {
+                this.transform.position = Vector2.MoveTowards(this.transform.position, exitScreen.position, movementSpeedExit * Time.deltaTime);
+                if (Vector2.Distance(this.transform.position, exitScreen.position) < 0.1f)
+                {
+
+                    yield return new WaitForSeconds(2);
+                    break;
+                }
+                yield return null;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Vector3 swipePositionLeft = swipePositionsLeft[UnityEngine.Random.Range(0, swipePositionsLeft.Length)].position;
+                Vector3 swipePositionRight = swipePostionsRight[UnityEngine.Random.Range(0, swipePostionsRight.Length)].position;
+                int choice = UnityEngine.Random.Range(0, 2);
+
+                Vector2 firstSwipePostion = Vector2.zero;
+                Vector2 lastSwipePosition = Vector2.zero;
+
+                switch (choice)
+                {
+                    case 0:
+                        firstSwipePostion = swipePositionLeft;
+                        lastSwipePosition = swipePositionRight;
+                        Debug.Log(firstSwipePostion);
+                        break;
+                    case 1:
+                        firstSwipePostion = swipePositionRight;
+                        lastSwipePosition = swipePositionLeft;
+                        Debug.Log(firstSwipePostion);
+                        break;
+                    default:
+                        firstSwipePostion = swipePositionLeft;
+                        Debug.Log(firstSwipePostion);
+                        break;
+                }
+                this.transform.position = firstSwipePostion;
+
+                while (MovePos(lastSwipePosition, movementSpeed * Time.deltaTime)) 
+                {
+                    yield return null;
+                } 
+            }
+
+            this.transform.position = defaultPosition;
+            waitingToEnd = true;
+        }
+
+    bool MovePos(Vector3 target, float moveSpeed)
     {
-        while (true)
+        this.transform.position =  Vector2.MoveTowards(this.transform.position, target, moveSpeed);
+        if (Vector2.Distance(this.transform.position, target) < 0.1f)
         {
-            this.transform.position = Vector2.MoveTowards(this.transform.position, exitScreen.position, movementSpeedExit * Time.deltaTime);
-            if (Vector2.Distance(this.transform.position, exitScreen.position) < 0.1f)
-            {
-
-                yield return new WaitForSeconds(2);
-                break;
-            }
-            yield return null;
+            return false;
         }
-
-        this.transform.position = swipePositions[0].position;
-
-        while (true)
-        {
-            this.transform.position = Vector2.MoveTowards(this.transform.position, swipePositions[1].position, movementSpeed * Time.deltaTime);
-            if (Vector2.Distance(this.transform.position, swipePositions[1].position) < 0.1f)
-            {
-                yield return new WaitForSeconds(2);
-                break;
-            }
-
-            yield return null;
-        }
-        this.transform.position = swipePositions[2].position;
-
-        while (true)
-        {
-            this.transform.position = Vector2.MoveTowards(this.transform.position, swipePositions[3].position, movementSpeed * Time.deltaTime);
-            if (Vector2.Distance(this.transform.position, swipePositions[3].position) < 0.1f)
-            {
-                this.transform.position = defaultPosition;
-                yield return new WaitForSeconds(2);
-                break;
-            }
-            yield return null;
-        }
-
-        this.transform.position = defaultPosition;
-        waitingToEnd = true;
-
+        return true;
     }
 
     void OnTriggerEnter2D(Collider2D collider2D)
