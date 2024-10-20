548be806b64b4f3cf7d6d64009c974bc6e67d9aa
FLKS07 <67174547+FLKS07@users.noreply.github.com>
2024-10-20 18:54:58 +0100

    out 20 18:54

--- Assets/Scripts/Player/Sword1.cs
+++ Assets/Scripts/Player/Sword1.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Sword1 : MonoBehaviour
 {
+    GameObject bg;
+    public AudioClip audioClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Detected a object");
         collision.gameObject.SendMessage("Sword1",SendMessageOptions.DontRequireReceiver);
+        bg = GameObject.Find("BackgroundMusic");
+        bg.GetComponent<AudioSource>().PlayOneShot(audioClip);
     }
 }
